004277c75ddcda5b7037c62f40053fd6ba739ddd
= <=>
2017-09-01 10:30:29 +0200

    changed links and text

--- data/resources/views/welcome.blade.php
+++ data/resources/views/welcome.blade.php
@@ -79,15 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Multiplayer Notepad
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/Profeten/TwitterClone">Github Repo</a>
+                    <a href="https://www.linkedin.com/in/johan-saldes-codehead">LinkedIn</a>
+                    <a href="https://codehead.se">Codehead</a>
+                    <a href="https://itkonsult.net">It-Konsult</a>
                 </div>
             </div>
         </div>
